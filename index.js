c35934ed34c4056962a734de8224ff9d4246bb11
vazkido <vazkid02@gmail.com>
2025-02-11 11:32:20 -0500

    initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    const
+    res.end("Hello World\n");
+}).listen(8080);
+
+console.log("Server running at http://localhosthost:8080/");
+
